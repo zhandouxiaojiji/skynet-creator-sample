774d1c2d3fb896ed5ffedde09261ae72bc265819
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2022-01-23 16:01:05 +0800

    import uuid

--- service/main.lua
+++ service/main.lua
@@ -1,9 +1,11 @@
 local skynet = require "skynet"
 local cjson = require "cjson.safe"
 local openssl = require "openssl"
+local uuid = require "uuid"
 
 skynet.start(function ()
     print "hello skynet!"
     print("test cjson", cjson.encode {aa = 1})
     print("test openssl", openssl.base64("hello"))
+    print("test uuid", uuid())
 end)
