e8b56fd2634fc8031faf2172dcea3cc22ccd1d92
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2022-02-03 09:45:24 +0800

    test crab

---
+++ examples/test_crab.lua
@@ -0,0 +1,19 @@
+local crab = require "crab.c"
+local utf8 = require "utf8.c"
+
+local words = {}
+for line in io.lines("3rd/crab/words.txt") do
+    local t = {}
+    assert(utf8.toutf32(line, t), "non utf8 words detected:"..line)
+    table.insert(words, t)
+end
+
+crab.open(words)
+
+local input = io.input("3rd/crab/texts.txt"):read("*a")
+local texts = {}
+assert(utf8.toutf32(input, texts), "non utf8 words detected:", texts)
+crab.filter(texts)
+local output = utf8.toutf8(texts)
+
+print(output)
