2e815cc0769451e5d0647e7ec96d9cc7b4250cc3
huangjiaxiang <jiaxiang.huang@centurygame.com>
2022-06-06 16:10:16 +0800

    test makefile

--- make/jps.mk
+++ make/jps.mk
@@ -7,4 +7,4 @@ ${JPS_SOURCE}:
 	git submodule update --init 3rd/jps
 
 ${BUILD_CLUALIB_DIR}/jps.so: ${JPS_SOURCE}
-	gcc $(CFLAGS) $(SHARED) -I3rd/jps/ $^ -o $@ $(LDFLAGS)
+	${CC} $(CFLAGS) $(SHARED) -I3rd/jps/ $^ -o $@
