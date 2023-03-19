63eb923202382236840657651a051719c3e537a0
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2023-03-19 12:19:20 +0800

    test openai completions

--- examples/test_openai.lua
+++ examples/test_openai.lua
@@ -1,14 +1,22 @@
 local skynet = require "skynet"
 local function test_openai()
     local api_key = skynet.getenv "openai_api_key"
+    local organization = skynet.getenv "openai_organization"
     if not api_key or api_key == "" then
         print "api_key not configure"
         return
     end
     local openai = skynet.newservice("openai")
     skynet.call(openai, "lua", "init", {
-        api_key = api_key
+        api_key = api_key,
+        organization = organization,
+        timeout = 6000,
     })
+    -- skynet.call(openai, "lua", "req_organization", "text-davinci-003")
+    local resp = skynet.call(openai, "lua", "completions", "Hello GPT")
+    local content = resp.choices[1].message.content
+    print("you:", "Hello GPT")
+    print("gpt:", content)
 end
 
 test_openai()
--- service/openai.lua
+++ service/openai.lua
@@ -1,22 +1,81 @@
 local skynet = require "skynet"
 local httpc = require "http.httpc"
 local cjson = require "cjson.safe"
+local cURL = require "cURL"
 
 local api_key
+local organization
 
 local function common_header()
     return {
-        ["Content-Type"] = "application/json",
-        ["Authorization"] = "Bearer " .. openai_api_key,
+        "Content-Type: application/json",
+        "Authorization: Bearer " .. api_key,
     }
+end
+
+local function post(url, body)
+    local header = common_header()
+    local resp
+
+    c = cURL.easy{
+        url = url,
+        post = true,
+        httpheader = header,
+        writefunction = function(data)
+            resp = data
+            -- print(data)
+        end,
+        postfields = cjson.encode(body)
+    }
+    c:perform()
+    c:close()
+
+    return resp and cjson.decode(resp) or nil
 end
 
 local CMD = {}
 function CMD.init(args)
     api_key = assert(args.api_key)
+    organization = args.organization
     if args.timeout then
         httpc.timeout = args.timeout
     end
+end
+
+function CMD.req_organization(model)
+    local header = {
+        "Authorization: Bearer " .. api_key,
+        "OpenAI-Organization:" .. organization
+    }
+
+    local url = "https://api.openai.com/v1/models"
+    if model then
+        url = url .. "/" .. model
+    end
+    local resp
+    c = cURL.easy{
+        url = url,
+        httpheader = {
+            "Authorization: Bearer " .. api_key,
+            "OpenAI-Organization: " .. organization
+        },
+        writefunction = function(data)
+            resp = data
+        end
+    }
+    c:perform()
+    c:close()
+    return resp and cjson.decode(resp) or nil
+end
+
+function CMD.completions(content)
+    local header = common_header()
+    local body = {
+        model = "gpt-3.5-turbo",
+        messages = {{role = "user", content = content}},
+        temperature = 0.7
+    }
+    return post("https://api.openai.com/v1/chat/completions", body)
 end
 
 skynet.start(function()
